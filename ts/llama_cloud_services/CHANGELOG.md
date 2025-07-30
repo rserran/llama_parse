09b192b98beb79945bc12ed870d562b16c126491
Clelia (Astra) Bertelli <133636879+AstraBert@users.noreply.github.com>
2025-07-30 17:59:08 +0200

    Adding TS llama-cloud-services and moving llama-parse to uv (#811)
    * wip: monorepo changes
    
    * fix ci for the time being
    
    * fix ci for the time being pt2
    
    * wip: first cloud refactoring for ts
    
    * chore: restore original package
    
    * fix: imports, package.json, tsconfig.json, client, reader
    
    * feat: adjustments after local testing
    
    * ci: github actions for typescript
    
    * ci: typescript ci
    
    * ci: nvmrc :facepalm:
    
    * ci: remove cache :facepalm:
    
    * ci: actions
    
    * ci: actions (i lost count)
    
    * ci: pnpm run format
    
    * ci: pnpm run format
    
    * chore: migrate llama-parse to uv
    
    * add tests
    
    * remove unneeded readme
    
    * update workflows
    
    * feat: modify py release workflow, adding uv version, bump version for llama-cloud-services to latest
    
    * uv lock
    
    * ci: python tests all tests
    
    * fix: lock file pulling in wrong version of numpy
    
    ---------
    
    Co-authored-by: Logan Markewich <logan.markewich@live.com>
    Co-authored-by: Adrian Lyjak <adrianlyjak@gmail.com>

---
+++ ts/llama_cloud_services/CHANGELOG.md
@@ -0,0 +1,5 @@
+# llama-cloud-services
+
+## v0.1.0
+
+First release for `llama-cloud-services`.
---
+++ ts/llama_cloud_services/src/index.ts
@@ -0,0 +1,8 @@
+export { LLamaCloudFileService } from "./LLamaCloudFileService.js";
+export { LlamaCloudIndex } from "./LlamaCloudIndex.js";
+export {
+  LlamaCloudRetriever,
+  type CloudRetrieveParams,
+} from "./LlamaCloudRetriever.js";
+export type { CloudConstructorParams } from "./type.js";
+export { LlamaParseReader } from "./reader.js";
