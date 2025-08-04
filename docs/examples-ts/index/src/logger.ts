98db5eeeae05ca057eaa129c9e1cc68404812203
Marcus Schiesser <marcus.schiesser@googlemail.com>
2025-08-04 17:48:24 +0800

    chore: remove llamaindex dep (#826)
    * chore: remove llamaindex dep
    
    * chore: remove all dependency on llamaindex
    
    * feat: restructure docs/examples
    
    * chore: remove llamaindex dep
    
    * chore: remove all dependency on llamaindex
    
    * simplify querytool
    
    * fix tests
    
    * revert version
    
    * add missing import
    
    * remove unused file
    
    * feat: change default description to adapt it to LlamaCloud Index
    
    ---------
    
    Co-authored-by: Clelia (Astra) Bertelli <clelia@runllama.ai>

--- docs/examples-ts/index/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import { createConsola } from "consola";
-import type { ConsolaInstance } from "consola";
-
-export const logger: ConsolaInstance = createConsola({
-  formatOptions: {
-    date: false,
-  },
-});
